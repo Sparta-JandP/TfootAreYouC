30615f8aacf50fef4f953a0045496dac12d48aa2
eohalim <dhl612@naver.com>
2023-09-25 21:35:11 +0900

    [CHORE]타입 이름 수정
    ->체력이름 Health로 변경

--- TfootAreYouC/Assets/Scripts/Enemy.cs
+++ TfootAreYouC/Assets/Scripts/Enemy.cs
@@ -2,25 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum EnemyType
+{
+    Spade,
+    Heart,
+    Clover,
+    Dia,
+    /*보스 카드 보류 
+    Jack,
+    Qeen,
+    King,
+    Jocker
+    */
+}
+
 public class Enemy : MonoBehaviour
 {
-    public enum EnemyType
-    {
-        Spade,
-        Heart,
-        Clover,
-        Dia,
-        /*보스 카드 보류 
-        Jack,
-        Qeen,
-        King,
-        Jocker
-        */
-    }
 
     public EnemyType type;
     public int Defensive;
-    public int Stamina;
+    public int Health;
     public int Speed;
     public int Attack;
 
@@ -30,28 +31,28 @@ public class Enemy : MonoBehaviour
         {
             case EnemyType.Spade:
                 Defensive = 1;
-                Stamina = 1;
+                Health = 1;
                 Speed = 1;
                 Attack = 1;
                 break;
 
             case EnemyType.Heart:
                 Defensive = 1;
-                Stamina = 1;
+                Health = 1;
                 Speed = 1;
                 Attack = 2;
                 break;
 
             case EnemyType.Clover:
                 Defensive = 2;
-                Stamina = 2;
+                Health = 2;
                 Speed = 1;
                 Attack = 2;
                 break;
 
             case EnemyType.Dia:
                 Defensive = 2;
-                Stamina = 3;
+                Health = 3;
                 Speed = 2;
                 Attack = 3;
                 break;
