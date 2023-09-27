e34f64d90953d1710665ebb46c08d809c9cfa730
HYEONJI LEE <8976guswl@naver.com>
2023-09-27 16:47:37 +0900

    [Add] StageClearPanel 추가

--- TfootAreYouC/Assets/Scripts/GameScene/GameSceneUIController.cs
+++ TfootAreYouC/Assets/Scripts/GameScene/GameSceneUIController.cs
@@ -1,18 +1,71 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameSceneUIController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [Header("SandClock")]
+    [SerializeField] private Image _mineral;
+    [SerializeField] private TMP_Text _mineralAmount;
+
+    [Header("Health")]
+    [SerializeField] private Image _kingHealthBar;
+    [SerializeField] private Image _trumpBossHealthBar;
+
+    [Header("Stage")]
+    [SerializeField] private TMP_Text _stageNumText;
+
+
+    private StageManager _stageManager;
+
+    int _maxMineral;
+    int _maxKingHealth;
+    int _maxTrumpBossHealth;
+
+    int _stageNum;
+    int _curMineral;
+    int _curKingHealth;
+    int _curTrumpBossHealth;
+
+
     void Start()
     {
-        
+        _stageManager = StageManager.instance;
+
+        _maxMineral = _stageManager.maxMineral;
+        _maxKingHealth = _stageManager.maxKingHealth;
+        _maxTrumpBossHealth = _stageManager.maxBossHealth;
+
+        _stageManager.OnStageClear += UpdateStageNumUI;
+        _stageManager.OnMining += UpdateSandAmountUI;
+        _stageManager.OnKingHealthChange += UpdateKingHealthUI;
+        _stageManager.OnBossHealthChange += UpdateTrumpBossHealthUI;
     }
 
-    // Update is called once per frame
-    void Update()
+    void UpdateStageNumUI()
     {
-        
+        _stageNum = _stageManager.currentStage; 
+        _stageNumText.text = _stageNum.ToString();
+    }
+
+    void UpdateSandAmountUI()
+    {
+        _curMineral = _stageManager.mineral;
+        _mineral.fillAmount = (float)_curMineral / _maxMineral;
+        _mineralAmount.text = _curMineral.ToString();
+    }
+
+    void UpdateKingHealthUI()
+    {
+        _curKingHealth = _stageManager.kingHealth;
+        _kingHealthBar.fillAmount = (float)_curKingHealth / _maxKingHealth;
+    }
+
+    void UpdateTrumpBossHealthUI()
+    {
+        _curTrumpBossHealth = _stageManager.bossHealth;
+        _trumpBossHealthBar.fillAmount = (float)_curTrumpBossHealth / _maxTrumpBossHealth;
     }
 }
