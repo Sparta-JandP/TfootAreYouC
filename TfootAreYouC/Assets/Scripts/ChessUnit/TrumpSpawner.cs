72c81dc6992a04214f114a15d95a1bf9cc111c9e
HYEONJI LEE <8976guswl@naver.com>
2023-09-30 21:01:58 +0900

    [Fix] TrumpSpawn 로직 수정

--- TfootAreYouC/Assets/Scripts/ChessUnit/TrumpSpawner.cs
+++ TfootAreYouC/Assets/Scripts/ChessUnit/TrumpSpawner.cs
@@ -1,22 +1,73 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class TrumpSpawner : MonoBehaviour
 {
-    public Transform[] spawnpoints;
+    [SerializeField] private Tilemap Tilemap;
+
+    public Vector3[] spawnPoints;
 
     public GameObject[] trumps = new GameObject[4];
 
+    private StageManager stageManager;
+    private int stageNum;
+    private int stageRate;
+
     private void Start()
     {
-        InvokeRepeating("SpawnTrump", 2, 5);
+        SetSpawnPoint();
+
+        stageManager = StageManager.instance;
+
+        //TODO: StartSpawning과 UpdateStage 이벤트에 연결하기
+        UpdateStage();
+        StartSpawning();
+    }
+
+    void SetSpawnPoint()
+    {
+        int start = Tilemap.cellBounds.min.y;
+        int width = Tilemap.cellBounds.max.x;
+        int height = Tilemap.cellBounds.size.y;
+
+        spawnPoints = new Vector3[height];
+
+        for (int i = 0; i < height; i++)
+        {
+            Vector3Int cellPosition = new Vector3Int(width - 1, start + i, 0);
+            Vector3 worldPosition = Tilemap.GetCellCenterWorld(cellPosition);
+            spawnPoints[i] = worldPosition;
+        }
+       
+    }
+    
+    void StartSpawning()
+    {
+        StartCoroutine("RepeateSpawnTrump");
+    }
+
+    void UpdateStage() 
+    {
+        StopCoroutine("RepeateSpawnTrump");
+        stageNum = stageManager.currentStage;
+        stageRate = 5 / stageNum; 
+    }
+
+    IEnumerator RepeateSpawnTrump()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(stageRate);
+            SpawnTrump();
+        }
     }
     void SpawnTrump()
     {
         int idx = Random.Range(0, 4);
-        int r = Random.Range(0, spawnpoints.Length);
-        GameObject myTrump = Instantiate(trumps[idx], spawnpoints[r].position, Quaternion.identity);
+        int r = Random.Range(0, spawnPoints.Length);
+        GameObject myTrump = Instantiate(trumps[idx], spawnPoints[r], Quaternion.identity);
     }
 
 }
