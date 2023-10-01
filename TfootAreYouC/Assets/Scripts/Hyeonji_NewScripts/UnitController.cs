a25b815efa6ce516c4e0e34484175335edac68f6
HYEONJI LEE <8976guswl@naver.com>
2023-10-01 22:36:40 +0900

    [Update] 상대방과 충돌 시 정지하도록

--- TfootAreYouC/Assets/Scripts/Hyeonji_NewScripts/UnitController.cs
+++ TfootAreYouC/Assets/Scripts/Hyeonji_NewScripts/UnitController.cs
@@ -15,8 +15,13 @@ public class UnitController : MonoBehaviour
     [HideInInspector]
     public float speed;
 
+    [SerializeField] private LayerMask target;
+
     private IEffect _effect;
     private HealthSystem _healthSystem;
+
+    private int _enemyCount = 0;
+    private bool _isFighting;
 
     private void Awake()
     {
@@ -32,6 +37,7 @@ public class UnitController : MonoBehaviour
 
     private void Start()
     {
+        StartCoroutine(SpeedControl());
         _healthSystem.OnDeath += Die;
         if (_effect != null)
             _effect.ApplyEffect(effectPower, effectRate);
@@ -54,5 +60,45 @@ public class UnitController : MonoBehaviour
     {
         yield return new WaitForSeconds(1.5f);
         Destroy(gameObject);
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if ((target.value == (target.value | (1 << other.gameObject.layer))))
+        {
+            _enemyCount++;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if ((target.value == (target.value | (1 << other.gameObject.layer))))
+        {
+            _enemyCount--;
+        }
+    }
+
+    IEnumerator SpeedControl()
+    {
+        while (true)
+        {
+            if (_enemyCount == 0)
+            {
+                if (_isFighting == true)
+                {
+                    speed = unitStats.speed;
+                    _isFighting = false;
+                }
+            }
+            else if (_enemyCount >= 1)
+            {
+                if (_isFighting == false)
+                {
+                    speed = 0;
+                    _isFighting = true;
+                }
+            }
+            yield return new WaitForSeconds(1f);
+        }
     }
 }
