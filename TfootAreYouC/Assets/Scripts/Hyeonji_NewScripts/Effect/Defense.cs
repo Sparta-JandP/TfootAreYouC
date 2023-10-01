145f82d87c3289081050d6605f96b07638b32c3d
HYEONJI LEE <8976guswl@naver.com>
2023-10-01 21:47:05 +0900

    [Add] Defense 기능 추가
    rook 프리팹 완성

---
+++ TfootAreYouC/Assets/Scripts/Hyeonji_NewScripts/Effect/Defense.cs
@@ -0,0 +1,70 @@
+using System;
+using System.Collections;
+using UnityEngine;
+
+public class Defense : MonoBehaviour, IEffect
+{
+    public event Action OnDefenseStart;
+    public event Action OnDefenseEnd;
+
+    private UnitController _controller;
+
+    [SerializeField] private LayerMask target;
+
+    private int _enemyCount = 0;
+    private float _speed;
+    private bool _isDefending;
+
+    private void Start()
+    {
+        _controller = GetComponent<UnitController>();
+        _speed = _controller.speed;
+    }
+
+    public void ApplyEffect(int power, float rate)
+    {
+        StartCoroutine(DefenseAction());
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if ((target.value == (target.value | (1 << other.gameObject.layer)))) 
+        {
+            _enemyCount++;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if ((target.value == (target.value | (1 << other.gameObject.layer))))
+        {
+            _enemyCount--;
+        }
+    }
+
+    IEnumerator DefenseAction()
+    {
+        while (true)
+        {
+            if (_enemyCount == 0)
+            {
+                if (_isDefending == true)
+                {
+                    OnDefenseEnd?.Invoke();  // Rook - 애니메이션에서 standing up 연결
+                    _isDefending = false;
+                    _controller.speed = _speed;
+                }
+            }
+            else if (_enemyCount >= 1)
+            {
+                if (_isDefending == false)
+                {
+                    OnDefenseStart?.Invoke(); // Rook - 애니메이션에서 sitting 연결
+                    _isDefending = true;
+                    _controller.speed = 0;
+                }
+            }
+            yield return new WaitForSeconds(1f);
+        }
+    }
+}
