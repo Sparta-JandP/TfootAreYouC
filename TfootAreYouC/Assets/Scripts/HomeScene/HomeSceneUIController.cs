58f35be6ca74a8ab0345b2de3abe1f0ec569859d
HYEONJI LEE <8976guswl@naver.com>
2023-10-03 16:12:41 +0900

    [Update] UI 연결 수정

---
+++ TfootAreYouC/Assets/Scripts/HomeScene/HomeSceneUIController.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class HomeSceneUIController : MonoBehaviour
+{
+    [SerializeField] private GameObject _upgradePanel;
+    [SerializeField] private GameObject _settingPanel;
+    [SerializeField] private TMP_Text _coinText;
+
+    [SerializeField] private Button muteButton;
+    [SerializeField] private Button muteOffButton;
+    [SerializeField] private Slider volumeSlider;
+
+
+    private void Start()
+    {
+        GameManager.instance.OnCoinChange += UpdateCoin;
+
+        SoundManager.instance.muteButton = this.muteButton;
+        SoundManager.instance.muteOffButton = this.muteOffButton;
+        SoundManager.instance.volumeSlider = this.volumeSlider;
+    }
+
+    private void UpdateCoin()
+    {
+        _coinText.text = GameManager.instance.Coin.ToString();
+    }
+
+    public void OpenUpgradePanel()
+    {
+        SoundManager.instance.PlayEffect("positive");
+        _upgradePanel.SetActive(true);
+    }
+
+    public void CloseUpgradePanel()
+    {
+        SoundManager.instance.PlayEffect("negative");
+        _upgradePanel.SetActive(false);
+    }
+
+    public void OpenSettingPanel()
+    {
+        SoundManager.instance.PlayEffect("positive");
+        _settingPanel.SetActive(true);
+    }
+
+    public void CloseSettingPanel()
+    {
+        SoundManager.instance.PlayEffect("negative");
+        _settingPanel.SetActive(false);
+    }
+
+    public void GameSceneChange()
+    {
+        SoundManager.instance.PlayEffect("positive");
+        SceneManager.LoadScene("GameScene");
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
